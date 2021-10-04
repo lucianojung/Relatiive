8ee101f7a8653d0254aa1a62aa603114475952f7
Luciano Jung <Luciano.jung@posteo.de>
2021-10-04 19:52:24 +0200

    add change between languages && therefore add asset localization database - bugfix resetted Images on start

--- Assets/Scripts/LocalizationChangeController.cs
+++ Assets/Scripts/LocalizationChangeController.cs
@@ -1,19 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 
 public class LocalizationChangeController : MonoBehaviour
 {
     public RelativeController relativeController;
+    public Sprite germanFlag;
+    public Sprite usaFlag;
     
+    private Image _image;
+    
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+        ChangeSelectedLocale();
+        ChangeSelectedLocale();
+    }
+
     public void ChangeSelectedLocale()
     {
         if (LocalizationSettings.SelectedLocale == LocalizationSettings.AvailableLocales.Locales[0])
         {
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+            _image.sprite = usaFlag;
         }
         else
         {
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+            _image.sprite = germanFlag;
         }
         relativeController.OnRelativeTapped(relativeController.CurrentRelative);
     }
